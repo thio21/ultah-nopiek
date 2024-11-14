85d0488f48ecfe4b7224e3715c6a14a4d8bf6138
thio21 <justitio.fadly@gmail.com>
2024-11-14 18:24:52 +0700

    Ultah Nopiek

---
+++ script.js
@@ -0,0 +1,3 @@
+function myFunction(){
+window.open("https://wa.me/6285719929110?text= Maacih beb, dah inget sama Ultah aku. Aku sayang banget sama byby. BTW aku boleh minta hadiahnya gak? xixixi");
+              }
